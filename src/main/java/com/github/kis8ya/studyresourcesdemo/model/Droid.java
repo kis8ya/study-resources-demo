972f207874fb37f2c81af2c84f54d963e09ecaaf
kis8ya <kis8P@yandex.ru>
2022-03-23 01:38:37 +0300

    added some examples for configuration management

---
+++ src/main/java/com/github/kis8ya/studyresourcesdemo/model/Droid.java
@@ -0,0 +1,12 @@
+package com.github.kis8ya.studyresourcesdemo.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Droid {
+
+    private String id;
+    private String description;
+}
